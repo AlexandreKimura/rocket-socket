65ec4315dede259f0e37c27b3a09c0649f8931cf
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-06-21 10:21:00 -0300

    add socket.io and static pages

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express  from "express";
+import path from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const io = new Server(server);
+
+io.on("connection", async (socket) => {
+  console.log("Socket", socket);
+})
 
 app.get("/", (request, response) =>{
   return response.json({
@@ -8,4 +21,4 @@ app.get("/", (request, response) =>{
   })
 })
 
-app.listen(3000, () => console.log("Server is running!"));
+server.listen(3000, () => console.log("Server is running!"));
